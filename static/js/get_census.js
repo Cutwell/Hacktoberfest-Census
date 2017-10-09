4b374993603aea4929926e9ff93a60f59b11811d
Zachary Smith <zacharysmith2000@googlemail.com>
2017-10-09 21:26:04 +0100

    added webpage

---
+++ static/js/get_census.js
@@ -0,0 +1,14 @@
+$.get(
+    "https://raw.githubusercontent.com/Cutwell/Hacktoberfest-Census/master/README.md",
+    function(file) {
+        var census_md = file.split("### Census")[1].split("\n");
+        census_md.map((user) => {
+            if(user == ""){
+                return;
+            }
+            var name = user.substring(user.lastIndexOf("[")+1, user.lastIndexOf("]"));
+            var url = user.substring(user.lastIndexOf("(")+1, user.lastIndexOf(")"));
+            $("#census-list").append("<li class='census-item'><a href="+url+">"+name+"</a></li>");
+        });
+    }
+);
